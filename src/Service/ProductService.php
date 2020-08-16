79e14161f5e0165a926be17abc8c353e903291d5
Jose Alberto Hernán <joserto86@gmail.com>
2020-08-16 17:49:18 +0200

    cambios en ProductService

--- src/Service/ProductService.php
+++ src/Service/ProductService.php
@@ -3,13 +3,15 @@
 namespace App\Service;
 
 use App\Entity\Product;
+use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
 
 class ProductService
 {
-    public function __construct(ProductRepository $productRepository)
+    public function __construct(ProductRepository $productRepository, CategoryRepository $categoryRepository)
     {
         $this->productRepository = $productRepository;
+        $this->categoryRepository = $categoryRepository;
     }
 
     public function getProductById(int $id) :?Product
@@ -29,6 +31,13 @@ class ProductService
 
     public function saveProduct(Product $product) :bool
     {
+        if ($product->getCategoryId() != null) {
+            $category = $this->categoryRepository->getCategoryById($product->getCategoryId());
+            if ($category != null) {
+                $product->setCategory($category);
+            }
+        }
+
         return $this->productRepository->saveProduct($product);
     }
 }
